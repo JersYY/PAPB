8d5be50908223a3332d582ab9a583de867d64940
JersYY <stevenanthony881@example.com>
2025-09-03 21:41:10 +0700

    Add Tugas 1

---
+++ Tugas 1/app/src/main/java/com/example/papb/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.papb
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.papb.ui.theme.PAPBTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PAPBTheme {
+                App()
+            }
+        }
+    }
+}
+
+@Composable
+fun App() {
+    // kosong = tampilan awal "Halo" saja
+    var name by rememberSaveable { mutableStateOf("") }
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Greeting(name = name)
+            Spacer(Modifier.height(16.dp))
+            Button(onClick = { name = "Steven Anthony" }) {
+                Text("Tampilkan Sapaan")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val message = if (name.isBlank()) "Halo" else "Halo $name!"
+    Text(text = message, modifier = modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppPreview() {
+    PAPBTheme { App() }
+}
